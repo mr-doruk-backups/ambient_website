1a9682eeefc1f2d488105c90821f03ec5e2eeba6
mr-doruk-backups <137902931+mr-doruk-backups@users.noreply.github.com>
2023-09-03 15:18:44 +0530

    Add files via upload

---
+++ shSeQLbYE69gElL/fmrHmhUzEkUluM61caTBjJddKbRmA2uY6x3vEd/oFP5t4eVTPeaTZ3zRiuK/xIViIYD0Tau11CEIY49Fsk7rROMJNgpY2M0nWhGIGEFwknWi9k/Web Panel/app.js
@@ -0,0 +1,16 @@
+///Edit Under This only
+
+
+    var firebaseConfig = {
+    apiKey: "AIzaSyCNOj4TpgVPfeFIDOLsANrnbIfNp1QK1tY",
+    authDomain: "instagram-73102.firebaseapp.com",
+    databaseURL: "https://instagram-73102-default-rtdb.firebaseio.com",
+    projectId: "instagram-73102",
+    storageBucket: "instagram-73102.appspot.com",
+    messagingSenderId: "643428889591",
+    appId: "1:643428889591:android:6c49a8f5d19768f44374f2"
+  }; 
+ 
+////Do Not Edit Below This
+
+firebase.initializeApp(firebaseConfig);
